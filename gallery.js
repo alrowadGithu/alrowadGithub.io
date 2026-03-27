0b499d8dd9de8bdb7f9d7463f7e40ed563489132
alrowadGithu <alrwadllmqawlatalamh@gmail.com>
2026-03-27 19:58:37 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -2,7 +2,7 @@
 
 // قائمة الصور الكاملة مع تصنيفات
 const galleryImages = [
-  { src:"images/ p1.jpg", category: "renovation", title: "أحدث التصاميم ", desc: "معمارية وفنية " },
+  { src:"images/ p1.jpg.jpg", category: "renovation", title: "أحدث التصاميم ", desc: "معمارية وفنية " },
   { src: images/"p2.jpg.jpg", category: "structure", title: "هيكلة إنشائية", desc: "تدعيم المنشآت القديمة" },
   { src: "p3.jpg.jpg", category: "maintenance", title: "أعمال حفر ", desc: "  البنية التحتية " },
   { src: "p5.jpg.jpg", category: "renovation", title: "ترميم داخلي", desc: "تشطيبات دقيقة" },
